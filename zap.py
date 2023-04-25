8edb6c2148a2c202cdea87b7e7afb3fe9a4578ee
Peshtik <126643522+Peshtik@users.noreply.github.com>
2023-04-26 00:05:40 +1000

    87 уколов по фаренгейту

--- zap.py
+++ zap.py
@@ -1,56 +1,56 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(366, 445)
+        MainWindow.resize(742, 475)
         MainWindow.setStyleSheet("background-color: rgb(0, 0, 0);\n"
 "color:white;")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.timeEdit = QtWidgets.QTimeEdit(self.centralwidget)
-        self.timeEdit.setGeometry(QtCore.QRect(10, 250, 321, 91))
-        self.timeEdit.setStyleSheet("background-color: rgb(255, 255, 255);\n"
-"font: 14pt \"MS Sans Serif\";\n"
-"color: rgb(0, 0, 0);")
-        self.timeEdit.setDateTime(QtCore.QDateTime(QtCore.QDate(2023, 4, 23), QtCore.QTime(10, 0, 0)))
-        self.timeEdit.setMaximumDateTime(QtCore.QDateTime(QtCore.QDate(2025, 4, 23), QtCore.QTime(18, 0, 0)))
-        self.timeEdit.setMaximumTime(QtCore.QTime(18, 0, 0))
-        self.timeEdit.setMinimumTime(QtCore.QTime(10, 0, 0))
-        self.timeEdit.setTime(QtCore.QTime(10, 0, 0))
-        self.timeEdit.setObjectName("timeEdit")
-        self.dateEdit = QtWidgets.QDateEdit(self.centralwidget)
-        self.dateEdit.setGeometry(QtCore.QRect(10, 110, 321, 121))
-        self.dateEdit.setStyleSheet("background-color: rgb(255, 255, 255);\n"
-"font: 14pt \"MS Sans Serif\";\n"
-"color: rgb(0, 0, 0);")
-        self.dateEdit.setDateTime(QtCore.QDateTime(QtCore.QDate(2023, 9, 23), QtCore.QTime(10, 0, 0)))
-        self.dateEdit.setMaximumDateTime(QtCore.QDateTime(QtCore.QDate(2025, 12, 31), QtCore.QTime(18, 0, 0)))
-        self.dateEdit.setMinimumDateTime(QtCore.QDateTime(QtCore.QDate(2023, 9, 23), QtCore.QTime(10, 0, 0)))
-        self.dateEdit.setMinimumTime(QtCore.QTime(10, 0, 0))
-        self.dateEdit.setObjectName("dateEdit")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(30, 30, 271, 61))
+        self.label.setGeometry(QtCore.QRect(100, 60, 521, 91))
         self.label.setStyleSheet("\n"
 "    color:white;\n"
 "    border: 3px solid #ff5500;\n"
 "    border-radius: 12;")
         self.label.setObjectName("label")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(60, 380, 201, 31))
+        self.pushButton.setGeometry(QtCore.QRect(140, 390, 451, 61))
         self.pushButton.setStyleSheet("QPushButton{\n"
-"background-color: rgb(255, 255, 255);\n"
-"color:black;\n"
-"border: 4px solid rgb(170, 0, 0) ;\n"
-"border-color: 13;}\n"
-"\n"
+"    background-color: white ;\n"
+"    color: rgb(0, 0, 0);\n"
+"    border: 3px solid #ff5500;\n"
+"    border-radiius:12;}\n"
 "QPushButton:hover{\n"
 "    background-color: rgb(85, 255, 255);}\n"
 "QPushButton:pressed{\n"
 "    background-color: rgb(85, 85, 127);}\n"
 "")
         self.pushButton.setObjectName("pushButton")
+        self.dateTimeEdit = QtWidgets.QDateTimeEdit(self.centralwidget)
+        self.dateTimeEdit.setGeometry(QtCore.QRect(20, 170, 671, 181))
+        font = QtGui.QFont()
+        font.setFamily("MS Sans Serif")
+        font.setPointSize(14)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
+        self.dateTimeEdit.setFont(font)
+        self.pushButton.pressed.connect(self.getinfo)
+        self.dateTimeEdit.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"font: 14pt \"MS Sans Serif\";\n"
+"color: rgb(0, 0, 0);\n"
+"\n"
+"")
+        self.dateTimeEdit.setDate(QtCore.QDate.currentDate())
+        self.dateTimeEdit.setTime(QtCore.QTime.currentTime())
+        self.dateTimeEdit.setMaximumDateTime(QtCore.QDateTime(QtCore.QDate(2023, 5, 30), QtCore.QTime(23, 59, 59)))
+        self.dateTimeEdit.setMinimumDateTime(QtCore.QDateTime(QtCore.QDate(2023, 4, 25), QtCore.QTime(10, 0, 0)))
+        self.dateTimeEdit.setCurrentSection(QtWidgets.QDateTimeEdit.HourSection)
+        self.dateTimeEdit.setCurrentSectionIndex(3)
+        self.dateTimeEdit.setTimeSpec(QtCore.Qt.TimeZone)
+        self.dateTimeEdit.setObjectName("dateTimeEdit")
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
@@ -61,6 +61,22 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.label.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:12pt;\">Выберете дату и время</span></p></body></html>"))
         self.pushButton.setText(_translate("MainWindow", "PushButton"))
+
+
+
+    def getinfo(self):
+        # dat = self.dateTimeEdit.date()
+        # ti = self.dateTimeEdit.time()
+        al = self.dateTimeEdit.dateTime()
+        self.insert(al)
+
+    def insert(self, x):
+        cursor.execute('''INSERT INTO database (data) VALUES (x)''')
+
+
+
+
+
 
 
 if __name__ == "__main__":
